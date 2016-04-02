5b37f6e8cf6a1f39cb347a5708323df97d4e0c73
Jennifer Valle <jvs1192@gmail.com>
2016-04-02 13:18:03 +0200

    First Commit

---
+++ Concatenacion.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Concatenacion
+{
+    class Concatenacion
+    {
+        static void Main(string[] args)
+        {
+            
+            /*var cadena= string.Empty;
+            for (int i = 0; i < 100000; i++) {
+
+                cadena += i;
+                
+                
+            }
+            Console.Write(cadena);
+            Console.ReadLine();*/
+
+            var sb = new StringBuilder();
+            for (int i=0; i < 10000; i++) {
+                sb.Append(i);  // clase especial para concatenar cadenas
+            }
+            Console.WriteLine(sb);
+            Console.ReadLine();
+
+        }
+    }
+}
